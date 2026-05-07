18d11d48e17c5d69301c6b23aac15dc37ec85698
KALAISELVAN-18 <kalai2808200@gmail.com>
2026-05-07 19:23:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'https://emergix-reporting.netlify.app',
     methods: ['GET', 'POST']
   }
 });
